7eff492ba57b5223ee9759c012541aa138ab3b2d
Md. Arafat Kbir <99961325+Arafat2222@users.noreply.github.com>
2023-07-11 02:26:23 +0600

    Accept

---
+++ Solution/Make all equal using Pairs.cpp
@@ -0,0 +1,43 @@
+///\/\/\/\/\/***************الله أكبر***************/\/\/\/\/\/\/
+//*********** حَسبنا الله و نعم الوکیل نعم المولی و نعم النصیر *************
+ 
+//1. بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
+//2. الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ
+//3. الرَّحْمَٰنِ الرَّحِيمِ
+//4. مَالِكِ يَوْمِ الدِّينِ
+//5. إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ
+//6. اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ
+//7. صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّينَ
+
+#include <bits/stdc++.h>
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define FOR(i, a, b) for (int i = a; i < b;i++)
+#define cin_v(v,n) loop(i,n) cin >> v[i]
+#define cout_v(v,n) loop(i,n) cout << v[i] << " "
+#define full(v) v.begin(),v.end()
+#define ll long long
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(false);
+  cin.tie(0),cout.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		map<int, int> mp;
+		for (int i = 1; i <= n; i++){
+			int x;
+			cin >> x;
+			mp[x]++;
+		}
+		int ans = INT_MIN;
+		for(auto u:mp){
+			ans = max(ans, u.second);
+		}
+		cout << n - ans << "\n";
+	}
+}
+
+//			 @@MD.Arafat Kabir@@	
+///\/\/\/\/\**********@University of Asia Pacific@**********/\/\/\/\/\/
